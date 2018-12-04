78b1cdbb14c0da647b6c5904a5a4928d19c033be
lorcannolan <c15513717@mydit.ie>
2018-12-04 13:06:37 +0000

    Added firebase connection

--- app/src/main/java/ie/dit/interimprototype/MapActivity.java
+++ app/src/main/java/ie/dit/interimprototype/MapActivity.java
@@ -7,12 +7,15 @@ import android.location.Address;
 import android.location.Geocoder;
 import android.nfc.Tag;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -24,6 +27,8 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -33,16 +38,21 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
     private static final String TAG = "MapActivity";
 
-    private boolean isPermissionGranted = false;
     private GoogleMap myMap;
-
     private EditText courseSearch;
+    private FloatingActionButton save;
+
+    private Address address;
+    private LatLng courseLatLang;
+
+    DatabaseReference courseRef = FirebaseDatabase.getInstance().getReference().child("courses");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
         courseSearch = (EditText) findViewById(R.id.searchText);
+        save = (FloatingActionButton) findViewById(R.id.save);
 
         initMap();
 
@@ -67,8 +77,9 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                     }
 
                     if (list.size() > 0) {
-                        Address address = list.get(0);
+                        address = list.get(0);
                         LatLng latlng = new LatLng(address.getLatitude(), address.getLongitude());
+                        courseLatLang = latlng;
                         Log.d(TAG, address.toString());
 
                         myMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng, 15f));
@@ -80,6 +91,19 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                     }
                 }
                 return false;
+            }
+        });
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+                String id = courseRef.push().getKey();
+                courseRef.child(id).child("name").setValue(courseSearch.getText().toString());
+                courseRef.child(id).child("Address").setValue(address.getAddressLine(0));
+                courseRef.child(id).child("location").setValue(courseLatLang);
             }
         });
     }
